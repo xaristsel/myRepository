f4c34af053df744972813582827b446821db15cb
Christos Peristeris <christos.peristeris@camelotgloballs.com>
2020-11-19 23:59:15 +0200

    [master] Initial commit for spring core examples codebase

---
+++ src/main/java/com/codehub/springcoreexamples/SpringCoreExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.codehub.springcoreexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreExamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codehub/springcoreexamples/SpringCoreExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codehub.springcoreexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
